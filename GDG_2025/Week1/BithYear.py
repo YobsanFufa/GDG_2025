fc5138b8024a21aff5348bc5b8db2b7c0a4e3ddb
Yobsan Fufa <yobsanfufa3@gmail.com>
2025-12-09 20:26:11 +0300

    Remove duplicate Week1 folder, keep only GDG_2025/WEEK1 structure

--- GDG_2025/Week1/BithYear.py
+++
@@ -1,8 +0,0 @@
-def Birthday(age, currentyear):
-    return currentyear-age
-
-
-age = int(input("Enter your age: "))
-currentyear = int(input("Enter Current Year: "))
-result = Birthday(age, currentyear)
-print("Your Birth Year is : ", result)
