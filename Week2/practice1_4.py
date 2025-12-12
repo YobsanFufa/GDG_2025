5d161d83cc8c7539df355fa7c1bae91a8fb64097
YobsanFufa <164784285+YobsanFufa@users.noreply.github.com>
2025-12-12 16:16:29 +0300

    Delete Week2/practice1_4.py

--- Week2/practice1_4.py
+++
@@ -1,9 +0,0 @@
-def get_grade(student_grades, student_name):
-    try:
-        return student_grades[student_name]  
-    except KeyError:
-        return "Student not found in the system"
-# Example usage:
-grades = {"Becky": 90,"Nimo": 82,"Moti": 87,}
-print(get_grade(grades, "Moti"))  
-print(get_grade(grades, "Alazer"))
